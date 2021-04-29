1f74c91e675774eb46b3d7e0b5089769629a4371
Shane Bird <76505008+Shane-Wedderburn@users.noreply.github.com>
2021-04-29 15:36:01 +1000

    Addec CORS

--- src/contexts/axiosConfig.tsx
+++ src/contexts/axiosConfig.tsx
@@ -7,7 +7,7 @@ const instance = axios.create({
   baseURL: "https://api.github.com/",
   headers: {
     Authorization: `Bearer ${token}`,
-    AccessControlAllowOrigin: "*"
+    "Access-Control-Allow-Origin": "*"
   },
 });
 
