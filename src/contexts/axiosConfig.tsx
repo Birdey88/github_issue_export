e6e8de24a85cb4218b2a92bb56e6ce9ced98fa28
Shane Bird <shane.bird88@gmail.com>
2021-04-29 20:22:47 +1000

    hopefully it is fixed for Vercel build

--- src/contexts/axiosConfig.tsx
+++ src/contexts/axiosConfig.tsx
@@ -7,8 +7,7 @@ const instance = axios.create({
   baseURL: "https://api.github.com/",
   headers: {
     Authorization: `Bearer ${token}`
-  },
-  withCredentials: true
+  }
 });
 
 export default instance;
